a81060f758e1b826d9465e375f855579d3717052
meredithdean <meredithdvhamilton@gmail.com>
2022-10-10 16:29:29 +0100

    Added methods to check location of player, hat, holes

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ const pathCharacter = '*';
 const Field = class {
     constructor(field = [[]]) {
         this.field = field
+        this.locationX = 0;
+        this.locationY = 0;
+        // home position before game starts
+        this.field[0][0] = pathCharacter;
     };
 
     print() {
@@ -15,6 +19,23 @@ const Field = class {
             return row.join('');
           }).join('\n');
         console.log(displayString);
+      }
+
+      isInBounds() {
+        return (
+          this.locationY >= 0 &&
+          this.locationX >= 0 &&
+          this.locationY < this.field.length &&
+          this.locationX < this.field[0].length
+        );
+      }
+
+      isHat() {
+        return this.field[this.locationY][this.locationX] === hat;
+      }
+
+      isHole() {
+        return this.field[this.locationY][this.locationX] === hole;
       }
 };
 
