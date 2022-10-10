91224a8bf19b0abc2e0328f02298f79556ed1935
meredithdean <meredithdvhamilton@gmail.com>
2022-10-10 17:50:57 +0100

    Add generateField() method for random field

--- main.js
+++ main.js
@@ -86,8 +86,28 @@ const Field = class {
         return this.field[this.locationY][this.locationX] === hole;
       }
 
-    
-};
+        static generateField(height, width, percentage = 0.1) {
+            const field = new Array(height).fill(0).map(el => new Array(width));
+            for (let y = 0; y < height; y++) {
+              for (let x = 0; x < width; x++) {
+                const prob = Math.random();
+                field[y][x] = prob > percentage ? fieldCharacter : hole;
+              }
+            }
+            // Set the "hat" location
+            const hatLocation = {
+              x: Math.floor(Math.random() * width),
+              y: Math.floor(Math.random() * height)
+            };
+            // Make sure the "hat" is not at the starting point
+            while (hatLocation.x === 0 && hatLocation.y === 0) {
+              hatLocation.x = Math.floor(Math.random() * width);
+              hatLocation.y = Math.floor(Math.random() * height);
+            }
+            field[hatLocation.y][hatLocation.x] = hat;
+            return field;
+          }
+      };
 
-
-console.log(this.field);
+const myfield = new Field(Field.generateField(10, 10, 0.2));
+myfield.runGame();
